54e01fede7e7d36dbc066867f58e5366fe025ef1
Michel Richard <mrichard4@LM3000026417.local>
2014-11-02 20:41:22 -0500

    switch to UMD

--- js/radio.communicator.js
+++ js/radio.communicator.js
@@ -1,25 +1,37 @@
-define([
-	"radio"
-],function( Radio ) {
+// UMD communicator
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['radio'], factory);
+    } else if (typeof exports === 'object') {
+        // Node(commonjs)
+        module.exports = factory(require('b'));
+    } else {
+        // Browser globals (root is window)
+        root.communicator = factory(root.b);
+    }
+}(this, function ( radio ) {
 
-		var Communicator = Marionette.Controller.extend({
-			initialize: function( options ) {
-				this.channels = {};
-			},
+	// communication controller
+	// wraps Radio
+    var Communicator = Marionette.Controller.extend({
+		initialize: function( options ) {
+			this.channels = {};
+		},
 
-			// creates a new channel
-			_createChannel: function( channel ){
-				this.channels[ channel ] = Radio.channel( channel );
-			},
+		// creates a new channel
+		_createChannel: function( channel ){
+			this.channels[ channel ] = Radio.channel( channel );
+		},
 
-			// gets a channel
-			getChannel: function( channel ){
-				if( !this.channels[ channel ] ) {
-					this._createChannel( channel );
-				}
-				return this.channels[ channel ];
+		// gets a channel
+		getChannel: function( channel ){
+			if( !this.channels[ channel ] ) {
+				this._createChannel( channel );
 			}
-		});
-
-		return new Communicator();
+			return this.channels[ channel ];
+		}
 	});
+
+	return new Communicator();
+}));
